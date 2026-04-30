7b64dbe744bef604f8ab72e70e8200872825b740
carolrealpe <carolyjav@gmail.com>
2026-04-30 16:24:13 -0500

    Agregado Header y Diseño de Home

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   baseUrl: 'https://api.themoviedb.org/3',
-  apiKey: 'TU_API_KEY_AQUI',
+  apiKey: '575dd81c257bdc29a7c64c8f46836dc4',
   imgPath: 'https://image.tmdb.org/t/p'
 };
