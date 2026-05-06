8a2a229e5e49d031d8747124bcf8b80867ac37f9
EstebanEsc2025 <johansebastianborda@gmail.com>
2026-05-06 14:29:05 -0500

    fix: corregir tipografía en API Key de TMDB

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   baseUrl: 'https://api.themoviedb.org/3',
-  apiKey: '8333ef164ef1a029c78278f2441a5477',
+  apiKey: '575dd81c257bdc29a7e64c8f46836de4',
   imgPath: 'https://image.tmdb.org/t/p'
 };
