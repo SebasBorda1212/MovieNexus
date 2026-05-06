5a95ce8d3815ff9601c8613fd0c28deee9620c0e
EstebanEsc2025 <johansebastianborda@gmail.com>
2026-05-06 14:23:16 -0500

    fix: actualizar API Key de TMDB

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   baseUrl: 'https://api.themoviedb.org/3',
-  apiKey: '575dd81c257bdc29a7c64c8f46836dc4',
+  apiKey: '8333ef164ef1a029c78278f2441a5477',
   imgPath: 'https://image.tmdb.org/t/p'
 };
