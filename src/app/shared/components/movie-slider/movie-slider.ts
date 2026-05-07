48dbaed49993b4b355a01abbeb10793bd710544d
SebasBorda1212 <johansebastianborda@gmail.com>
2026-05-07 16:34:47 -0500

    feat: añadir flechas de navegación al carrusel y corregir home

--- src/app/shared/components/movie-slider/movie-slider.ts
+++ src/app/shared/components/movie-slider/movie-slider.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Movie } from '../../../core/models/movie.model';
 import { MovieCard } from '../movie-card/movie-card';
@@ -13,4 +13,19 @@ import { MovieCard } from '../movie-card/movie-card';
 export class MovieSlider {
   @Input({ required: true }) movies: Movie[] = [];
   @Input() title: string = '';
+
+  // Referencia al contenedor de las tarjetas
+  @ViewChild('sliderTrack') sliderTrack!: ElementRef<HTMLDivElement>;
+
+  // Función para mover el carrusel
+  scroll(direction: 'left' | 'right') {
+    const track = this.sliderTrack.nativeElement;
+    const scrollAmount = track.clientWidth * 0.8; // Movemos el 80% del ancho visible
+
+    if (direction === 'left') {
+      track.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+    } else {
+      track.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+    }
+  }
 }
