c1fec0c28bf7674bf6ea85b75080c08e42df97b4
EstebanEsc2025 <estebanescarpetta@gmail.com>
2026-06-24 16:55:17 -0500

    fix: import RouterOutlet, Header y ChatWidgetComponent in App module

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,9 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Header } from './shared/components/layout/header/header';
-import { ChatWidgetComponent } from './shared/components/chat-widget/chat-widget.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Header, ChatWidgetComponent],
+  imports: [RouterOutlet, Header, ChatWidgetComponent, HttpClientModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
