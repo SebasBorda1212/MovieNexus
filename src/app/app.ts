d461fcfda76c51251a83c4ec0370d4d5825e7d95
EstebanEsc2025 <estebanescarpetta@gmail.com>
2026-06-24 16:56:15 -0500

    fix: import RouterOutlet, Header y ChatWidgetComponent en App (resuelve error de compilación)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,3 +1,7 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from './shared/components/layout/header/header';
+import { ChatWidgetComponent } from './shared/components/chat-widget/chat-widget.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @Component({
